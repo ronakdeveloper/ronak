77f442eabf3471dae17f8e5b1391fb3dded0de7a
ronakdeveloper <ronakthedeveloper@gmail.com>
2025-02-11 17:56:05 +0530

    update

--- Java/FirstClass.java
+++ Java/FirstClass.java
@@ -1,5 +1,8 @@
 class FirstClass {
     public static void main(String args[]) {
         System.out.println("Hello World");
+        System.out.println("This is my new line");
+        System.out.print("How are you! ");
+        System.out.print("I am Fine!!");
     }
 }
